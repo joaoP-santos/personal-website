66ce2ef6967deb225f23ebb90985515cb1904ec0
João Pedro Santos <santosjoaopedroms@gmail.com>
2024-10-28 11:06:42 -0300

    fixed useHead

--- app.vue
+++ app.vue
@@ -13,4 +13,31 @@ function scrollToSection(sectionId) {
   const section = document.querySelector(sectionId);
   section.scrollIntoView({ behavior: "smooth" });
 }
+
+useHead({
+  title: "João Pedro Santos",
+  meta: [
+    {
+      name: "description",
+      content: "João Pedro Marques Santos's personal website.",
+    },
+    {
+      name: "keywords",
+      content:
+        "Education, technology, AI, web development, social impact, programming",
+    },
+    {
+      property: "og:title",
+      content: "João Pedro Santos",
+    },
+    {
+      property: "og:description",
+      content: "João Pedro Marques Santos's personal website.",
+    },
+    {
+      property: "og:url",
+      content: "https://joaopedrosantos.com",
+    },
+  ],
+});
 </script>
